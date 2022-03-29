2408034ee0680c1e8e28052a24067d2c13f0989c
Omar Baabouj <baabouj.dev@gmail.com>
2022-03-29 11:36:12 +0000

    :bug: Use ENV variables

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -12,8 +12,7 @@ class Database
 
     private function __construct()
     {
-//        $this->pdo = new \PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
-        $this->pdo = new \PDO('mysql:host=localhost;dbname=dev', 'root', '');
+        $this->pdo = new \PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
 
