ca5ac1622eac888062b721218cc454d40a66356b
Omar Baabouj <baabouj.dev@gmail.com>
2022-05-24 09:22:09 +0100

    :bug: FIX: jwt decode payload changes data with '_' to '/'

--- src/Auth/JWT.php
+++ src/Auth/JWT.php
@@ -2,11 +2,16 @@
 
 namespace Pexess\Auth;
 
+use Pexess\Exceptions\UnauthorizedException;
+
 class JWT
 {
-    public static function generate(array $payload,string $key): string
+    public static function generate(array $payload, string $key, array $options = []): string
     {
-        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+
+        $payload["iat"] = time();
+        $payload["exp"] = $options['expires'] ?? time() + 60 * 15;
 
         $payload = json_encode($payload);
 
@@ -14,35 +19,46 @@ class JWT
 
         $base64UrlPayload = self::base64url_encode($payload);
 
-        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $key, true);
+        $signature = self::hmac_sha256($base64UrlHeader . "." . $base64UrlPayload, $key);
 
         $base64UrlSignature = self::base64url_encode($signature);
 
         return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
     }
 
-    public static function verify(string $token, string $key): array|false
+    public static function verify(string $token, string $key, bool $ignore_expiration = false): array|false
     {
         $token = explode(".", $token);
-        if (count($token) != 3){
-            return false;
+        if (count($token) != 3) {
+            throw new UnauthorizedException();
         }
-        $payload = json_decode(self::base64url_decode($token[1]),true);
-        $signature = hash_hmac('sha256', $token[0] . "." . $token[1], $key, true);
+        $payload = json_decode(self::base64url_decode($token[1]), true);
+        $signature = self::hmac_sha256($token[0] . "." . $token[1], $key);
 
         $base64UrlSignature = self::base64url_encode($signature);
 
-        if (hash_equals($token[2], $base64UrlSignature)) return $payload;
-        return false;
+        if (hash_equals($token[2], $base64UrlSignature)) {
+            if (!$ignore_expiration && ($payload["exp"] < time())) {
+                throw new UnauthorizedException();
+            }
+            return $payload;
+        }
+
+        throw new UnauthorizedException();
     }
 
     private static function base64url_encode(string $string): string
     {
-        return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($string));
+        return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
     }
 
     private static function base64url_decode(string $string): string
     {
-        return str_replace(['-', '_', ''], ['+', '/', '='], base64_decode($string));
+        return base64_decode(strtr($string, '-_', '+/'));
+    }
+
+    private static function hmac_sha256(string $data, string $key): bool|string
+    {
+        return hash_hmac('sha256', $data, $key, true);
     }
 }
