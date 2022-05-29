ea72cc6fd3f5759024719f4a6f67b92dc3e0c93c
Omar Baabouj <baabouj.dev@gmail.com>
2022-05-29 14:37:16 +0100

    :heavy_plus_sign: NEW: add exposeHeaders and credentials to Cors

--- src/Security/Cors.php
+++ src/Security/Cors.php
@@ -4,9 +4,28 @@ namespace Pexess\Security;
 
 class Cors
 {
-
-    public static function origin($origin)
+    public static function set(array $cors): void
     {
+        $cors['origin'] && self::origin($cors['origin']);
+
+        $cors['headers'] && self::headers($cors['headers']);
+
+        $cors['methods'] && self::methods($cors['methods']);
+
+        $cors['maxAge'] && self::maxAge($cors['maxAge']);
+
+        $cors['exposeHeaders'] && self::exposeHeaders($cors['exposeHeaders']);
+
+        $cors['credentials'] && self::credentials($cors['credentials']);
+    }
+
+    public static function origin($origin): void
+    {
+        if (is_bool($origin)) {
+            $origin && header("Access-Control-Allow-Origin: *");
+            return;
+        }
+
         if (is_string($origin) && func_num_args() > 1) {
             $origin = func_get_args();
         }
@@ -16,19 +35,14 @@ class Cors
             $origin = in_array($http_origin, $origin) ? $http_origin : $origin[0];
         }
 
-        if (is_bool($origin)) {
-            $origin = $origin ? "*" : "";
-        }
-
         header("Access-Control-Allow-Origin: $origin");
-
-        return self::class;
     }
 
-    public static function headers($headers)
+    public static function headers($headers): void
     {
         if (is_bool($headers)) {
-            $headers = $headers ? "*" : "";
+            $headers && header("Access-Control-Allow-Headers: *");
+            return;
         }
 
         if (is_string($headers) && func_num_args() > 1) {
@@ -38,13 +52,10 @@ class Cors
         if (is_array($headers)) $headers = implode(", ", $headers);
 
         header("Access-Control-Allow-Headers: " . $headers);
-
-        return self::class;
     }
 
-    public static function methods($methods)
+    public static function methods($methods): void
     {
-
         if (is_string($methods) && func_num_args() > 1) {
             $methods = func_get_args();
         }
@@ -52,14 +63,31 @@ class Cors
         if (is_array($methods)) $methods = implode(", ", $methods);
 
         header("Access-Control-Allow-Methods: " . $methods);
-
-        return self::class;
     }
 
-    public static function maxAge($maxAge)
+    public static function maxAge($maxAge): void
     {
         header("Access-Control-Allow-MaxAge: $maxAge");
+    }
 
-        return self::class;
+    public static function exposeHeaders($exposeHeaders): void
+    {
+        if (is_bool($exposeHeaders)) {
+            $exposeHeaders && header("Access-Control-Expose-Headers: *");
+            return;
+        }
+
+        if (is_string($exposeHeaders) && func_num_args() > 1) {
+            $exposeHeaders = func_get_args();
+        }
+
+        if (is_array($exposeHeaders)) $exposeHeaders = implode(", ", $exposeHeaders);
+
+        header("Access-Control-Expose-Headers: " . $exposeHeaders);
+    }
+
+    public static function credentials(bool $allowCredentials = true): void
+    {
+        $allowCredentials && header("Access-Control-Allow-Credentials: true");
     }
 }
